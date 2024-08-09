20abd3f8875c92c4b5fec26ff57daa734ad83fc4
rezanahi <raad.thunder1@gmail.com>
2024-08-09 20:03:41 +0330

    add Menu Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import Menu from './components/Menu/Menu'
 
 function App() {
   return (
     <BrowserRouter>
-
+      <Menu></Menu>
     </BrowserRouter>
   );
 }
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,20 @@
+import './Menu.css'
+
+
+
+function Menu() {
+
+
+    return(
+        <div className="container">
+            <nav className='menu'>
+                <ul className='menu__list'>
+                    <li className='menu__list__item'>Home</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+
+export default Menu;
