f8e9ee09cd4c2a44f54ac25f966eb67561b303e2
rezanahi <raad.thunder1@gmail.com>
2024-12-09 01:06:34 +0330

    add Birthday.js page and add routes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import Menu from './components/Menu/Menu'
 
+//Pages
+import Birthday from './pages/1-birthday/Birthday'
+
 function App() {
   return (
     <BrowserRouter>
       <Menu></Menu>
+        <Routes>
+            <Route path="/1-birthday" element={<Birthday />} />
+
+        </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/pages/1-birthday/Birthday.js
@@ -0,0 +1,16 @@
+import './Birthday.css'
+import './Birthday-media.css'
+
+
+function Birthday () {
+
+
+    return(
+        <>
+            <p>I am the birthday</p>
+        </>
+    )
+}
+
+
+export default Birthday
