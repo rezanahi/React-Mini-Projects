db4c17a79e12e4f4d9c562cf35adba1e6c8d76a9
rezanahi <raad.thunder1@gmail.com>
2024-12-09 01:15:18 +0330

    create list component

---
+++ src/components/List/List.jsx
@@ -0,0 +1,18 @@
+import './List.css'
+import './List-media.css'
+
+
+
+function List () {
+
+
+    return(
+        <>
+            <p>List component</p>
+        </>
+    )
+}
+
+
+
+export default List
