86853e1704bf9d1ee7ba3e79ff590f2af8790881
rezanahi <raad.thunder1@gmail.com>
2024-12-14 22:51:16 +0330

    complete project 3

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,13 +1,61 @@
 import './Review.css'
 import './Review-media.css'
+import {useState} from "react";
 
+import ReviewsData from '../../pages/3-reviews/ReviewsData'
+
+// Icons
+import { FaQuoteRight, FaChevronLeft, FaChevronRight} from "react-icons/fa";
 
 function Review () {
+    const [index, setIndex] = useState(0)
+    const reviewData = ReviewsData[index]
 
+    function nextReviewHandler() {
+        if (index === ReviewsData.length - 1) {
+            setIndex(0)
+        } else {
+            setIndex(prevState => prevState + 1)
+        }
+    }
+
+    function previousReviewHandler() {
+        if(index === 0) {
+            setIndex(ReviewsData.length - 1)
+        } else {
+            setIndex(prevState => prevState - 1)
+        }
+    }
+
+    function randomReviewHandler() {
+        const randomNumber = Math.floor(Math.random() * ReviewsData.length);
+        setIndex(randomNumber)
+    }
 
     return(
         <>
-
+            <article className='p3-review'>
+                <div className='p3-img-container'>
+                    <img src={reviewData.image} alt={reviewData.name} className='p3-person-img'/>
+                    <span className='p3-quote-icon'>
+                        <FaQuoteRight></FaQuoteRight>
+                    </span>
+                </div>
+                <h4 className='p3-author'>{reviewData.name}</h4>
+                <p className='p3-job'>{reviewData.job}</p>
+                <p className='p3-info'>{reviewData.text}</p>
+                <div className='p3-button-container'>
+                    <button className='p3-prev-btn' onClick={previousReviewHandler}>
+                        <FaChevronLeft></FaChevronLeft>
+                    </button>
+                    <button className='p3-next-btn' onClick={nextReviewHandler}>
+                        <FaChevronRight></FaChevronRight>
+                    </button>
+                </div>
+                <button className='p3-random-btn' onClick={randomReviewHandler}>
+                    Surprise Me
+                </button>
+            </article>
         </>
     )
 }
--- src/pages/3-reviews/Reviews.jsx
+++ src/pages/3-reviews/Reviews.jsx
@@ -1,13 +1,24 @@
 import './Reviews.css'
 import './Reviews-media.css'
 
+// Components
+import Review from '../../components/Review/Review'
+
 
 function Reviews () {
 
 
     return(
         <>
-
+            <main className='p3-main'>
+                <section className='p3-container'>
+                    <div className='p3-title'>
+                        <h2>Our Reviews</h2>
+                        <div className='p3-underline'></div>
+                    </div>
+                    <Review></Review>
+                </section>
+            </main>
         </>
     )
 }
