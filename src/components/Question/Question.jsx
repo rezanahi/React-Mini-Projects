5ad90be499cd6f9aeae3738bf68d977acd9a891e
rezanahi <raad.thunder1@gmail.com>
2024-12-15 23:47:21 +0330

    complete project 4

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,14 +1,24 @@
 import './Question.css'
 import './Question-media.css'
 
+import {useState} from 'react'
+import {AiOutlineMinus, AiOutlinePlus} from 'react-icons/ai'
 
+function Question ({title, info}) {
 
-function Question () {
-
+    const [showInfo, setShowInfo] = useState(false)
 
     return(
         <>
-
+            <article className='p4-question'>
+                <header>
+                    <h4>{title}</h4>
+                    <button className='p4-btn' onClick={() => {setShowInfo(si => !si)}}>
+                        {showInfo ? <AiOutlineMinus></AiOutlineMinus> : <AiOutlinePlus></AiOutlinePlus>}
+                    </button>
+                </header>
+                {showInfo && <p>{info}</p>}
+            </article>
         </>
     )
 }
--- src/pages/4-accordion/Accordion.jsx
+++ src/pages/4-accordion/Accordion.jsx
@@ -1,13 +1,26 @@
 import './Accordion.css'
 import './Accordion-media.css'
-
+import AccordionData from "./AccordionData";
+import {useState} from 'react'
+import Question from "../../components/Question/Question";
 
 function Accordion () {
-
+    const [accordionData, setAccordionData] = useState(AccordionData)
 
     return(
         <>
-            Accordion
+            <main className='p4-main'>
+                <div className='p4-container'>
+                    <h3>Question and answers about login</h3>
+                    <section className='p4-info'>
+                        {
+                            accordionData.map((question) => {
+                                return <Question key={question.id} {...question}></Question>
+                            })
+                        }
+                    </section>
+                </div>
+            </main>
         </>
     )
 }
