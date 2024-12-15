49a550d0d1b16d085e1dc24cf61fb00e9266ff38
rezanahi <raad.thunder1@gmail.com>
2024-12-16 01:37:48 +0330

    create FoodMenuCategories component

---
+++ src/components/FoodMenuCategories/FoodMenuCategories.jsx
@@ -0,0 +1,16 @@
+import './FoodMenuCategories.css'
+import './FoodMenuCategories-media.css'
+
+
+
+function FoodMenuCategories () {
+
+
+    return(
+        <>
+            FoodMenuCategories
+        </>
+    )
+}
+
+export default FoodMenuCategories
