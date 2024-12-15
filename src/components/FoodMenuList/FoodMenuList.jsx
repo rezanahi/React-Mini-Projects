4f7fb1651a17175aa23f30eecdd6d1a091db9ce2
rezanahi <raad.thunder1@gmail.com>
2024-12-16 01:37:30 +0330

    create FoodMenuList component

---
+++ src/components/FoodMenuList/FoodMenuList.jsx
@@ -0,0 +1,15 @@
+import './FoodMenuList.css'
+import './FoodMenuList-media.css'
+
+
+function FoodMenuList() {
+
+    return(
+        <>
+            FoodMenuList
+        </>
+    )
+}
+
+
+export default FoodMenuList
