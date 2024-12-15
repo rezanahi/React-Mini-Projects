deb7636e7528492b60705404f81d7430a33424e4
rezanahi <raad.thunder1@gmail.com>
2024-12-16 01:51:10 +0330

    complete FoodMenuList.jsx component

--- src/components/FoodMenuList/FoodMenuList.jsx
+++ src/components/FoodMenuList/FoodMenuList.jsx
@@ -2,11 +2,29 @@ import './FoodMenuList.css'
 import './FoodMenuList-media.css'
 
 
-function FoodMenuList() {
+function FoodMenuList({menu}) {
 
     return(
         <>
-            FoodMenuList
+            <div className='p5-section-center'>
+                {
+                    menu.map((menuItem) => {
+                        const {id, title, img, desc, price} = menuItem
+                        return(
+                            <article key={id} className='p5-menu-item'>
+                                <img src={img} alt={title} className='p5-photo' />
+                                <div className='p5-item-info'>
+                                    <header>
+                                        <h4>{title}</h4>
+                                        <h4 className='p5-price'>{price}</h4>
+                                    </header>
+                                    <p className='p5-item-text'>{desc}</p>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
+            </div>
         </>
     )
 }
