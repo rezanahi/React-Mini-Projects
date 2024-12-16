db51ed92944c746d11a45e650b8e566bffc8abba
rezanahi <raad.thunder1@gmail.com>
2024-12-16 19:30:06 +0330

    complete project 6

--- src/pages/6-tabs/Tabs.jsx
+++ src/pages/6-tabs/Tabs.jsx
@@ -1,9 +1,10 @@
 import './Tabs.css'
 import {useState, useEffect} from "react";
+import tabsData from "./TabsData";
+import {FaAngleDoubleRight} from "react-icons/fa";
 
 
 const url = 'https://course-api.com/react-tabs-project'
-import tabsData from "./TabsData";
 
 function Tabs() {
     const [loading, setLoading] = useState(false);
@@ -13,7 +14,39 @@ function Tabs() {
 
     return(
         <>
-
+            <section className='p6-section bg-cyan-50'>
+                <div className='p6-title'>
+                    <h2>Experience</h2>
+                    <div className='p6-underline'></div>
+                </div>
+                <div className='p6-jobs-center'>
+                    <div className='p6-btn-container'>
+                        {jobs.map((job, index) => {
+                            return <button
+                                key={job.id}
+                                onClick={() => {setTab(index)}}
+                                className={`p6-job-btn ${index === tab && 'p6-active-btn'}`}>
+                                {job.company}
+                            </button>
+                        })}
+                    </div>
+                    <article className='p6-job-info'>
+                        <h3>{jobs[tab].title}</h3>
+                        <h4>{jobs[tab].company}</h4>
+                        <p className='p6-job-date'>{jobs[tab].dates}</p>
+                        {
+                            jobs[tab].duties.map((duty, index) => {
+                            return (
+                                <div key={index} className='p6-job-desc'>
+                                    <FaAngleDoubleRight className='p6-job-icon'></FaAngleDoubleRight>
+                                    <p>{duty}</p>
+                                </div>
+                            )
+                        })
+                        }
+                    </article>
+                </div>
+            </section>
         </>
     )
 }
