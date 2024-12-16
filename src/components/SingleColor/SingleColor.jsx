9b094285f71480d970b33503518e768a1e636f36
rezanahi <raad.thunder1@gmail.com>
2024-12-17 02:03:30 +0330

    complete project 9

--- src/components/SingleColor/SingleColor.jsx
+++ src/components/SingleColor/SingleColor.jsx
@@ -6,12 +6,20 @@ import rgbToHex from "../../pages/9-color/ColorUtils";
 function SingleColor({rgb, weight, index}) {
     const [alert, setAlert] = useState(false)
 
+    function onClickHandler(e) {
+        setAlert(true)
+        setTimeout(() => {
+            setAlert(false)
+        }, 2000)
+        navigator.clipboard.writeText(rgbToHex(...rgb))
+    }
 
     return(
         <>
-            <article className={`p9-color ${index > 10 && 'p9-color-light'}`} style={{backgroundColor: `rgb(${rgb.join(',')})`}}>
+            <article onClick={onClickHandler} className={`p9-color ${index > 10 && 'p9-color-light'}`} style={{backgroundColor: `rgb(${rgb.join(',')})`}}>
                 <p className='p9-percent-value'>{weight}%</p>
                 <p className='p9-color-value'>{rgbToHex(...rgb)}</p>
+                {alert && <p className='p9-alert'>Copied to clipboard</p>}
             </article>
         </>
     )
