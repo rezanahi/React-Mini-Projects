4509e1bffba8a3f3232f5864a3e19176efde855e
rezanahi <raad.thunder1@gmail.com>
2024-12-16 23:10:11 +0330

    complete project 8

--- src/pages/8-lorem/Lorem.jsx
+++ src/pages/8-lorem/Lorem.jsx
@@ -1,13 +1,41 @@
 import './Lorem.css'
 import loremData from './LoremData'
+import {useState} from 'react'
 
 function Lorem () {
+    const [count, setCount] = useState(0)
+    const [text, setText] = useState([])
 
+    function handleSubmit(e) {
+        e.preventDefault()
+        setText(loremData.slice(0, +count))
+    }
 
+    function handleCount(e) {
+        setCount(e.target.value)
+    }
 
     return(
         <>
-            Lorem Ipsum
+            <section className='p8-section-center'>
+                <h3>Tired of boring lorem ipsum?</h3>
+                <form className='p8-lorem-form' onSubmit={handleSubmit}>
+                    <label className='p8-label' htmlFor='amount'>
+                        paragraphs:
+                    </label>
+                    <input className='p8-input' type='number' min={0} name='amount' id='amount' value={count} onChange={handleCount}/>
+                    <button type='submit' className='p8-btn'>Generate</button>
+                </form>
+                <article className='p8-lorem-text'>
+                    {text.map(p => {
+                        return (
+                            <p>
+                                {p}
+                            </p>
+                        )
+                    })}
+                </article>
+            </section>
         </>
     )
 }
