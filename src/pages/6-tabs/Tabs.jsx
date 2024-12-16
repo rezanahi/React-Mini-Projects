5641164171728e7abf06bd82708ac773ad14aa6d
rezanahi <raad.thunder1@gmail.com>
2024-12-16 19:06:26 +0330

    add styles and data

--- src/pages/6-tabs/Tabs.jsx
+++ src/pages/6-tabs/Tabs.jsx
@@ -3,8 +3,12 @@ import {useState, useEffect} from "react";
 
 
 const url = 'https://course-api.com/react-tabs-project'
+import tabsData from "./TabsData";
 
 function Tabs() {
+    const [loading, setLoading] = useState(false);
+    const [jobs, setJobs] = useState(tabsData);
+    const [tab, setTab] = useState(0);
 
 
     return(
