b099aff453fbaa97c869b1b195e942416b22a32a
rezanahi <raad.thunder1@gmail.com>
2024-12-19 20:16:12 +0330

    complete hero structure

--- src/components/StripeHero/StripeHero.jsx
+++ src/components/StripeHero/StripeHero.jsx
@@ -1,12 +1,31 @@
 import './StripeHero.css'
+import {useStripeAppContext} from "../../pages/13-stripe/context";
 
 
 function StripeHero () {
-
+    const {closeModal} = useStripeAppContext()
 
     return(
         <>
-            Hero Component
+            <section className='p13-hero'>
+                <div className='p13-hero-center'>
+                    <article className='p13-hero-info'>
+                        <h1>
+                            Payment infrastructure for the internet
+                        </h1>
+                        <p>
+                            Millions of companies of all sizes-from startup to Fortune 500s-use
+                            Stripe's software and APIs to accept payments, send payouts, and manage their businesses online
+                        </p>
+                        <button className='p13-btn'>
+                            Start Now
+                        </button>
+                    </article>
+                    <article className='p13-hero-images'>
+                        <img src='/images/phone.svg' className='p13-phone-img' alt='phone'/>
+                    </article>
+                </div>
+            </section>
         </>
     )
 }
