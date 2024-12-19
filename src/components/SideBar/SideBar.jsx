1e6e5340d8b66519318aef3e7294c12cdabf1f11
rezanahi <raad.thunder1@gmail.com>
2024-12-19 17:38:27 +0330

    complete SideBar.jsx general structure

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,13 +1,39 @@
 import './SideBar.css'
 
-
-
+import {sideBarLinks, sideBarSocial} from '../../pages/12-sidebar-project/SideBarProjectData'
+import logo from './logo.svg'
+import {FaTimes} from "react-icons/fa";
 function SideBar () {
 
 
     return(
         <>
-            SideBar Component
+            <aside className={`p12-sidebar`}>
+                <div className='p12-sidebar-header'>
+                    <img src={logo} className='p12-logo' alt='logo'/>
+                    <button className='p12-close-btn'>
+                        <FaTimes></FaTimes>
+                    </button>
+                </div>
+                <ul className='p12-links'>
+                    {sideBarLinks.map((link) => {
+                        return (
+                            <li key={link.id}>
+                                <a href={link.url}>{link.icon}{link.text}</a>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <ul className='p12-social-icons'>
+                    {sideBarSocial.map((social) => {
+                        return (
+                            <li key={social.id}>
+                                <a href={social.url}>{social.icon}</a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </aside>
         </>
     )
 }
