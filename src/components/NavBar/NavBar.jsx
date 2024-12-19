b507f9182359c57a8f1b93538e31d928a0dabf1b
rezanahi <raad.thunder1@gmail.com>
2024-12-19 16:02:05 +0330

    add feature animating height of menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,23 @@
 import './NavBar.css'
 
 import logo from './logo.svg'
-import {FaBars, FaTwitter} from "react-icons/fa";
+import {FaBars} from "react-icons/fa";
 import {navBarLinks, navBarSocial} from "../../pages/11-navbar-project/NavBarProjectData";
+import {useEffect, useRef, useState} from "react";
 function NavBar () {
 
+    const [showLinks, setShowLinks] = useState(false)
+    const linksContainerRef = useRef(null)
+    const linksRef = useRef(null)
+
+    useEffect(() => {
+        let linksHeight = linksRef.current.clientHeight;
+        if(showLinks) {
+            linksContainerRef.current.style.height = linksHeight + 'px'
+        } else {
+            linksContainerRef.current.style.height = 0 + 'px'
+        }
+    }, [showLinks])
 
     return(
         <>
@@ -12,19 +25,21 @@ function NavBar () {
                 <div className='p11-nav-center'>
                     <div className='p11-nav-header'>
                         <img src={logo} alt='logo'/>
-                        <button className='p11-nav-toggle'>
+                        <button className='p11-nav-toggle' onClick={() => setShowLinks(!showLinks)}>
                             <FaBars></FaBars>
                         </button>
                     </div>
-                    <div className='p11-links-container p11-show-container'>
-                        <ul className='p11-links'>
-                            {navBarLinks.map((link) => {
-                                return (
-                                    <li key={link.id}><a href={link.url}>{link.text}</a></li>
-                                )
-                            })}
-                        </ul>
-                    </div>
+
+                        <div className={`p11-links-container`} ref={linksContainerRef}>
+                            <ul className='p11-links' ref={linksRef}>
+                                {navBarLinks.map((link) => {
+                                    return (
+                                        <li key={link.id}><a href={link.url}>{link.text}</a></li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
+
                     <ul className='p11-social-icons'>
                         {navBarSocial.map((link) => {
                             return (
