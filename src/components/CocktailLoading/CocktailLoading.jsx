d3074918a81dfa1bfac1a1631af746b83a38bb90
rezanahi <raad.thunder1@gmail.com>
2024-12-22 18:44:46 +0330

    add components

---
+++ src/components/CocktailLoading/CocktailLoading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CocktailLoading = () => {
+    return (
+        <div className="loader">
+        </div>
+    )
+}
+
+export default CocktailLoading
---
+++ src/components/CocktailNavbar/CocktailNavbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../logo.svg'
+
+const CocktailNavbar = () => {
+    return (
+        <div>
+            <h2>Cocktail navbar component</h2>
+        </div>
+    )
+}
+
+export default CocktailNavbar
