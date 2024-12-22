e390b8dd19d9dd3bb6f5b25ea3ad1dd2c64e81f8
rezanahi <raad.thunder1@gmail.com>
2024-12-22 21:34:53 +0330

    complete fetch from api by searchTerm

--- src/pages/15-cocktails/Cocktails.jsx
+++ src/pages/15-cocktails/Cocktails.jsx
@@ -5,12 +5,42 @@ import {BrowserRouter, Outlet, Route, Routes} from "react-router-dom";
 import CocktailNavbar from "../../components/CocktailNavbar/CocktailNavbar";
 import CocktailsHome from "./pages/CocktailsHome";
 import CocktailsAbout from "./pages/CocktailsAbout";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function Cocktails () {
     const [loading, setLoading] = useState(true);
     const [searchTerm, setSearchTerm] = useState("a");
     const [cocktails, setCocktails] = useState([]);
+
+    const fetchCocktails = async () => {
+        setLoading(true)
+        try {
+            const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+            const data = await response.json()
+            const {drinks} = data
+            if(drinks) {
+                setCocktails(drinks.map(drink => {
+                    const {idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass} = drink
+                    return {
+                        id: idDrink,
+                        name: strDrink,
+                        image: strDrinkThumb,
+                        info: strAlcoholic,
+                        glass: strGlass}
+                }))
+            } else {
+                setCocktails([])
+            }
+            setLoading(false)
+        } catch (err) {
+            console.log('error in fetch')
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        fetchCocktails()
+    }, [searchTerm]);
 
     return(
         <>
