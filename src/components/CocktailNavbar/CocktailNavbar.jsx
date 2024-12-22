930bc0b364cb144161db1df83b56aaf150130924
rezanahi <raad.thunder1@gmail.com>
2024-12-22 20:35:02 +0330

    complete CocktailNavbar.jsx component

--- src/components/CocktailNavbar/CocktailNavbar.jsx
+++ src/components/CocktailNavbar/CocktailNavbar.jsx
@@ -3,10 +3,21 @@ import { Link } from 'react-router-dom'
 
 const CocktailNavbar = () => {
     return (
-        <div>
-            <img src='/CocktailLogo.svg'/>
-            <h2>Cocktail navbar component</h2>
-        </div>
+        <nav className='p15-navbar'>
+            <div className='p15-nav-center'>
+                <Link to='/15-cocktails'>
+                    <img src='/CocktailLogo.svg' alt='cocktail db logo' className='p15-logo'/>
+                </Link>
+                <ul className='p15-nav-links'>
+                    <li>
+                        <Link to='/15-cocktails'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/15-cocktails/about'>About</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
