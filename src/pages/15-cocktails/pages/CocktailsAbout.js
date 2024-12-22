0ed30eadf81ffc5e4868728a9208aaed7a63cc2f
rezanahi <raad.thunder1@gmail.com>
2024-12-22 20:44:32 +0330

    complete about page

--- src/pages/15-cocktails/pages/CocktailsAbout.js
+++ src/pages/15-cocktails/pages/CocktailsAbout.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const CocktailsAbout = () => {
     return (
-        <div>
-            <h2>Cocktails about page</h2>
-        </div>
+        <>
+            <section className='p15-section p15-about-section'>
+                <h1 className='p15-section-title'>about us</h1>
+                <p>
+                    Lorem ipsum odor amet, consectetuer adipiscing elit. Varius dignissim suscipit lobortis dolor ipsum maximus; euismod cursus. Consequat dis hac risus proin proin consectetur. Nostra turpis eget lorem libero himenaeos fames netus potenti. Maximus tortor facilisi morbi etiam enim nisl. Litora litora morbi condimentum accumsan tempor suspendisse. Aliquam at adipiscing vel diam curae eleifend elementum scelerisque vivamus. Ultrices interdum natoque pretium id pharetra consectetur congue eu himenaeos. Accumsan commodo orci non donec et viverra tellus augue pellentesque?
+
+                    Taciti phasellus egestas volutpat vivamus ante. Varius aliquam tempus a auctor pretium sit. Egestas justo etiam nostra pellentesque sollicitudin aliquam vestibulum. Dignissim molestie facilisis a convallis augue quis auctor sagittis. Senectus fames aenean nec mauris maecenas nostra dui metus nunc. Scelerisque fringilla suscipit dictumst lectus dapibus iaculis. Turpis vestibulum taciti natoque quisque venenatis litora. Consectetur laoreet rhoncus elementum odio leo netus.
+                </p>
+            </section>
+        </>
     )
 }
 
