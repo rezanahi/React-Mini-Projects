05699af56217f801f50b0c6b9951f096701fca71
rezanahi <raad.thunder1@gmail.com>
2024-12-22 20:44:41 +0330

    complete error page

--- src/pages/15-cocktails/pages/CocktailsError.js
+++ src/pages/15-cocktails/pages/CocktailsError.js
@@ -3,9 +3,16 @@ import { Link } from 'react-router-dom'
 
 const CocktailsError = () => {
     return (
-        <div>
-            <h2>error page</h2>
-        </div>
+        <>
+            <section className='p15-error-page p15-section'>
+                <div className='p15-error-container'>
+                    <h1>oops! it's a dead end</h1>
+                    <Link to='/15-cocktails' className='p15-btn p15-btn-primary'>
+                        back home
+                    </Link>
+                </div>
+            </section>
+        </>
     )
 }
 
