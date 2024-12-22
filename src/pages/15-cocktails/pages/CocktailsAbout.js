b0c4c767acf00fbc76e5704262d9823df2528555
rezanahi <raad.thunder1@gmail.com>
2024-12-22 18:40:29 +0330

    add pages

---
+++ src/pages/15-cocktails/pages/CocktailsAbout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CocktailsAbout = () => {
+    return (
+        <div>
+            <h2>Cocktails about page</h2>
+        </div>
+    )
+}
+
+export default CocktailsAbout
---
+++ src/pages/15-cocktails/pages/CocktailsHome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import CocktailList from '../components/CocktailList'
+// import SearchForm from '../components/SearchForm'
+
+const CocktailsHome = () => {
+    return (
+        <div>
+            <h2>Cocktails home page</h2>
+        </div>
+    )
+}
+
+export default CocktailsHome
