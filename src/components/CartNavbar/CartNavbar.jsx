374648ea16286e43b1e42f11f6a8dfb2f8fe510f
rezanahi <raad.thunder1@gmail.com>
2024-12-22 18:05:43 +0330

    fix navbar amount bug

--- src/components/CartNavbar/CartNavbar.jsx
+++ src/components/CartNavbar/CartNavbar.jsx
@@ -5,7 +5,10 @@ import {useCartContext} from "../../pages/14-cart-project/CartContext";
 
 function CartNavbar() {
     const {cart} = useCartContext();
-
+    let amount = 0
+    cart.forEach(item => {
+        amount += item.amount
+    })
     return(
         <>
             <nav className='p14-nav'>
@@ -16,7 +19,7 @@ function CartNavbar() {
                             <path d='M16 6v2h2l2 12H0L2 8h2V6a6 6 0 1 1 12 0zm-2 0a4 4 0 1 0-8 0v2h8V6zM4 10v2h2v-2H4zm10 0v2h2v-2h-2z' />
                         </svg>
                         <div className='p14-amount-container'>
-                            <p className='p14-total-amount'>{cart.length}</p>
+                            <p className='p14-total-amount'>{amount}</p>
                         </div>
                     </div>
                 </div>
