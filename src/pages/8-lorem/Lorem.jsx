1e18070cd8dcc8b639be1394c968091ac4a34ba8
rezanahi <raad.thunder1@gmail.com>
2025-01-13 00:24:53 +0330

    correction project 8 : correct direction on texts add main tag and bg color

--- src/pages/8-lorem/Lorem.jsx
+++ src/pages/8-lorem/Lorem.jsx
@@ -17,25 +17,30 @@ function Lorem () {
 
     return(
         <>
-            <section className='w-[90vw] mx-auto max-w-[40rem] text-center mt-20'>
-                <h3>Tired of boring lorem ipsum?</h3>
-                <form className='capitalize mt-8 mb-16 flex justify-center items-center' onSubmit={handleSubmit}>
-                    <label className='text-xl' htmlFor='amount'>
-                        paragraphs:
-                    </label>
-                    <input className='py-1 px-2 w-16 rounded border-none mx-2 text-xl' type='number' min={0} max={9} name='amount' id='amount' value={count} onChange={handleCount}/>
-                    <button type='submit' className='uppercase bg-emerald-500 hover:bg-emerald-300 text-emerald-950 py-1 px-3 inline-block transition text-sm border-2 border-solid border-emerald-500 hover:border-emerald-300 cursor-pointer shadow rounded'>Generate</button>
-                </form>
-                <article className='last:mb-16'>
-                    {text.map(p => {
-                        return (
-                            <p>
-                                {p}
-                            </p>
-                        )
-                    })}
-                </article>
-            </section>
+            <main className='w-full min-h-screen bg-emerald-100 py-20 ' dir='ltr'>
+                <section className='w-[90vw] mx-auto max-w-[40rem] text-center'>
+                    <h3>Tired of boring lorem ipsum?</h3>
+                    <form className='capitalize mt-8 mb-16 flex justify-center items-center' onSubmit={handleSubmit}>
+                        <label className='text-xl' htmlFor='amount'>
+                            paragraphs:
+                        </label>
+                        <input className='py-1 px-2 w-16 rounded border-none mx-2 text-xl' type='number' min={0} max={9}
+                               name='amount' id='amount' value={count} onChange={handleCount}/>
+                        <button type='submit'
+                                className='uppercase bg-emerald-500 hover:bg-emerald-300 text-emerald-950 py-1 px-3 inline-block transition text-sm border-2 border-solid border-emerald-500 hover:border-emerald-300 cursor-pointer shadow rounded'>Generate
+                        </button>
+                    </form>
+                    <article>
+                        {text.map(p => {
+                            return (
+                                <p>
+                                    {p}
+                                </p>
+                            )
+                        })}
+                    </article>
+                </section>
+            </main>
         </>
     )
 }
