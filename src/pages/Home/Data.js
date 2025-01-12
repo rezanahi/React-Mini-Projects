9acb05ceb0cd05b9d6c357fa6c30034b86df1b89
rezanahi <raad.thunder1@gmail.com>
2025-01-12 23:36:09 +0330

    add projects data

---
+++ src/pages/Home/Data.js
@@ -0,0 +1,19 @@
+let projects = [
+    {id: 1, name: 'تاریخ تولد', level: 'ساده', image: './images/project-1.png', url: '/1-birthday'},
+    {id: 2, name: 'تاریخ تولد', level: 'ساده', image: './images/project-1.png', url: '/1-birthday'},
+    {id: 3, name: 'تاریخ تولد', level: 'ساده', image: './images/project-1.png', url: '/1-birthday'},
+    {id: 4, name: 'تاریخ تولد', level: 'ساده', image: './images/project-1.png', url: '/1-birthday'},
+    {id: 5, name: 'تاریخ تولد', level: 'ساده', image: './images/project-1.png', url: '/1-birthday'},
+    {id: 6, name: 'تاریخ تولد', level: 'ساده', image: './images/project-1.png', url: '/1-birthday'},
+    {id: 7, name: 'تاریخ تولد', level: 'ساده', image: './images/project-1.png', url: '/1-birthday'},
+    {id: 8, name: 'تاریخ تولد', level: 'ساده', image: './images/project-1.png', url: '/1-birthday'},
+    {id: 9, name: 'تاریخ تولد', level: 'ساده', image: './images/project-1.png', url: '/1-birthday'},
+    {id: 10, name: 'تاریخ تولد', level: 'ساده', image: './images/project-1.png', url: '/1-birthday'},
+    {id: 11, name: 'تاریخ تولد', level: 'ساده', image: './images/project-1.png', url: '/1-birthday'},
+    {id: 12, name: 'تاریخ تولد', level: 'ساده', image: './images/project-1.png', url: '/1-birthday'},
+    {id: 13, name: 'تاریخ تولد', level: 'ساده', image: './images/project-1.png', url: '/1-birthday'},
+    {id: 14, name: 'تاریخ تولد', level: 'ساده', image: './images/project-1.png', url: '/1-birthday'},
+]
+
+
+export default projects
