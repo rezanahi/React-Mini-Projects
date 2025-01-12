f1c262b67754fe85703c39efd5c4290d97518d43
rezanahi <raad.thunder1@gmail.com>
2025-01-12 23:36:39 +0330

    design home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
 import './Home.css'
 import './Home-media.css'
 import {Link} from 'react-router-dom';
-
+import HomePageCard from "../../components/HomePageCard/HomePageCard";
+import projects from "./Data";
 
 function Home () {
 
@@ -26,6 +27,15 @@ function Home () {
                 <li><Link to="/14-cart">14-cart</Link></li>
                 <li><Link to="/15-cocktails">15-cocktails</Link></li>
             </ul>
+            <div className='container px-2 md:px-0 mx-auto mb-16 w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 auto-rows-auto'>
+                {
+                    projects.map((item) => {
+                        return(
+                            <HomePageCard {...item} key={item.id}></HomePageCard>
+                        )
+                    })
+                }
+            </div>
         </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}", "./public/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
     extend: {
       translate: {
@@ -8,15 +8,33 @@ module.exports = {
       },
       colors: {
         gold: {
-          500: '#c59d5f'
+          200: '#dcc49f',
+          500: '#c59d5f',
         }
       },
       width: {
         '90-screen': '90vw',
+        'full-64px': 'calc(100% - 64px)',
+      },
+      aspectRatio: {
+        '1/2' : 1 / 2,
       },
       gridTemplateColumns: {
         '2-auto-fr': 'auto 1fr',
         '2-200px-fr': '200px 1fr',
+        'auto-fit-cols': 'repeat(auto-fit, minmax(223.33px, 1fr))',
+      },
+      gridTemplateRows: {
+        'auto-fit-rows': 'repeat(auto-fit, minmax(96px, 1fr))',
+      },
+      gridAutoColumns: {
+        'auto-fit-cols': 'minmax(223.33px, 1fr)',
+      },
+      gridAutoRows: {
+        'auto-fit-rows': 'minmax(96px, 1fr)',
+      },
+      fontFamily: {
+        'vazir': ['Vazirmatn']
       }
     },
   },
