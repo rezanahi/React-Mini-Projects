685128575c360aa2bdc6c4ae0ae5d75f88742f91
rezanahi <raad.thunder1@gmail.com>
2025-01-21 17:28:03 +0330

    complete homepagecard design

--- src/components/HomePageCard/HomePageCard.jsx
+++ src/components/HomePageCard/HomePageCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { BsArrowUpLeftSquareFill } from "react-icons/bs";
 
 
-function HomePageCard({ level, name, image, url, github}) {
+function HomePageCard({ level, name, image, url, github, discription}) {
   return (
     <div className="overflow-hidden w-full h-auto flex flex-col gap-4 border border-1 border-stone-500 rounded-md p-2 shadow-md bg-sto">
       <div className="w-full aspect-video bg-black overflow-hidden shrink-0 rounded border border-1 border-stone-500">
@@ -12,15 +12,18 @@ function HomePageCard({ level, name, image, url, github}) {
           className="w-full h-full object-cover"
         />
       </div>
-      <div className="flex w-full justify-between items-start">
+      <div className="flex flex-col w-full justify-between items-start">
         <Link to={url} className="w-full-64px block">
-          <h1 className="text-stone-800 font-vazir font-bold text-2xl mb-4 truncate">
+          <h1 className="text-stone-800 hover:text-stone-700 transition font-vazir font-bold text-2xl mb-4 truncate">
             {name}
           </h1>
         </Link>
+        <p className="text-stone-600">
+          {discription}
+        </p>
       </div>
-      <footer className="pt-2 border-t border-stone-500 flex justify-between items-center gap-2">
-        <Link to={github} className="py-1 px-2 grow rounded-md bg-stone-800  text-stone-400 font-extralight flex justify-between items-center">
+      <footer className="pt-2 mt-auto border-t border-stone-500 flex justify-between items-center gap-2">
+        <Link to={github} className="py-1 px-2 grow rounded-md bg-stone-800 hover:bg-stone-900 transition duration-500 text-stone-400 font-extralight flex justify-between items-center">
           مشاهده کد
           <BsArrowUpLeftSquareFill className="fill-stone-500"/>
         </Link>
