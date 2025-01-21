4d5c00b348746cbad91890465a544c9af4f7420e
rezanahi <raad.thunder1@gmail.com>
2025-01-22 02:24:58 +0330

    convert styles to tailwind

--- src/components/StripeSidebar/StripeSidebar.jsx
+++ src/components/StripeSidebar/StripeSidebar.jsx
@@ -10,21 +10,23 @@ function StripeSidebar () {
 
     return(
         <>
-            <aside className={`${isSidebarOpen ? 'p13-sidebar-wrapper p13-sidebar-show' :'p13-sidebar-wrapper'}`}>
-                <div className='p13-sidebar'>
-                    <button className='p13-close-btn' onClick={closeSidebar}>
+            <aside className={`fixed top-0 left-0 w-full h-full grid place-items-center -z-[1] transition-all scale-0 bg-stone-950/50 ${isSidebarOpen ? 'visible z-[2] scale-100' : 'invisible'} `}>
+                <div className='w-[90vw] h-[95vh] max-w-[1170px] bg-white rounded shadow relative py-16 px-8'>
+                    <button className='text-3xl bg-transparent border-transparent text-gray-700 hover:text-gray-900 transition absolute top-6 right-6 cursor-pointer' onClick={closeSidebar}>
                         <FaTimes></FaTimes>
                     </button>
-                    <div className='p13-sidebar-sublinks'>
+                    <div className='grid grid-rows-3 gap-1'>
                         {stripeSubLinks.map((link, index) => {
                             return (
-                                <article key={index}>
+                                <article className='mb-8' key={index}>
                                     <h4>{link.page}</h4>
-                                    <div className='p13-sidebar-sublinks'>
+                                    <div className='grid grid-rows-3 gap-1'>
                                         {link.links.map((sublink, index) => {
                                             return(
-                                                <a key={index} href={sublink.href}>
-                                                    {sublink.icon}
+                                                <a className='text-gray-700 capitalize flex items-center' key={index} href={sublink.href}>
+                                                    <span className='ml-4 fill-gray-700'>
+                                                        {sublink.icon}
+                                                    </span>
                                                     {sublink.label}
                                                 </a>
                                             )
