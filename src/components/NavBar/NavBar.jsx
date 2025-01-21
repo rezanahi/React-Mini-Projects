308836e8c692851179b37e1da81afc72acbca6b5
rezanahi <raad.thunder1@gmail.com>
2025-01-21 21:01:58 +0330

    convert project-11 styles to tailwind

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -21,29 +21,29 @@ function NavBar () {
 
     return(
         <>
-            <nav className='p11-nav'>
-                <div className='p11-nav-center'>
-                    <div className='p11-nav-header'>
+            <nav className='bg-white shadow'>
+                <div className='md:max-w-[1170px] md:mx-auto md:flex md:items-center md:justify-between md:p-4'>
+                    <div className='md:p-0 flex items-center justify-between p-4'>
                         <img src={logo} alt='logo'/>
-                        <button className='p11-nav-toggle' onClick={() => setShowLinks(!showLinks)}>
+                        <button className='text-2xl text-emerald-500 bg-transparent border-transparent transition cursor-pointer hover:text-emerald-700 hover:rotate-90 md:hidden' onClick={() => setShowLinks(!showLinks)}>
                             <FaBars></FaBars>
                         </button>
                     </div>
 
-                        <div className={`p11-links-container`} ref={linksContainerRef}>
-                            <ul className='p11-links' ref={linksRef}>
+                        <div className={`overflow-hidden transition-all md:!h-auto`} ref={linksContainerRef}>
+                            <ul className='md:flex' ref={linksRef}>
                                 {navBarLinks.map((link) => {
                                     return (
-                                        <li key={link.id}><a href={link.url}>{link.text}</a></li>
+                                        <li key={link.id}><a className='text-bg-gray-700 text-lg capitalize block py-2 px-4 hover:bg-emerald-200 hover:text-emerald-900 hover:pl-6 md:p-0 md:my-0 md:mx-2 md:hover:p-0 md:hover:bg-transparent' href={link.url}>{link.text}</a></li>
                                     )
                                 })}
                             </ul>
                         </div>
 
-                    <ul className='p11-social-icons'>
+                    <ul className='hidden md:flex md:gap-4'>
                         {navBarSocial.map((link) => {
                             return (
-                                <li key={link.id}><a href={link.url}>{link.icon}</a></li>
+                                <li key={link.id}><a className='md:text-emerald-600 md:transition md:hover:text-emerald-500' href={link.url}>{link.icon}</a></li>
                             )
                         })}
                     </ul>
